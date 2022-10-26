d201a3b46407d569a38d4e7301e415ed2cbebf60
Nicoly <nicolycarvalh15@gmail.com>
2022-10-25 20:47:37 -0400

    Substituindo atributos estáticos por props

--- src/components/GameBanner/index.tsx
+++ src/components/GameBanner/index.tsx
@@ -11,15 +11,14 @@ interface GameBannnerProps {
  *          atributos estáticos das imagem (src), titulo e contador de anuncios pelas respectivas props
  */
 
-export function GameBanner() {
+export function GameBanner(props: GameBannnerProps) {
   return (<><a href="" className="relative rounded-lg overflow-hidden">
-    <img src="https://static-cdn.jtvnw.net/ttv-boxart/32982_IGDB-188x250.jpg" alt="" />
-
-    <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-      <strong className="font-bold text-white block">GTA V</strong>
-      <span className="text-zinc-300 text-sm block">3 anuncio(s)</span>
-    </div>
+  <img src={props.imagem} alt="" />
+  
+  <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+  <strong className="font-bold text-white block">{props.titulo}</strong>
+  <span className="text-zinc-300 text-sm block">{props.contador}</span>
+  </div>
   </a></>);
-}
-
+  }
 export default GameBanner;
