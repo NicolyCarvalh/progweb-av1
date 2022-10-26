58457fef78d0b47cf635a8493cbb396ead0fa57c
Nicoly <nicolycarvalh15@gmail.com>
2022-10-25 20:55:37 -0400

    Criando variavel de estado 'games'

--- src/App.tsx
+++ src/App.tsx
@@ -17,11 +17,15 @@ function App() {
    * (2pt) Crie uma variável de estado com o nome 'games' e defina como inicialmente um array vazio
    */
 
+  const games = useState([])
+
   useEffect(() => {
     fetch('http://localhost:3333/games').then(response => response.json()).then(data => {
       /**
        * (1pt) atribua o conteúdo 'data' para a variável games
        */
+
+      
     }
     )
   }, [])
